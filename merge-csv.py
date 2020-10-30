7c21f0200f9ee70fae48c106e6a0500b784555bf
manescianera <nakas.zygimantas@gmail.com>
2020-10-30 11:09:10 +0200

    config for windows

--- merge-csv.py
+++ merge-csv.py
@@ -1,19 +1,30 @@
 import os
 import glob
 import pandas as pd
+import pathlib
 import csv
+import time
 
-# wdir = '/Users/zgs_prsnl/Documents/projects/merge-csv/'
-wdir = ''
+wdir = str(pathlib.Path().absolute())
+rmdir = wdir + '\\rm_lines'
+bindir = wdir + '\\bin'
 extension = 'csv'
-os.chdir(wdir + 'rm_lines')
+
+if not os.path.exists(rmdir):
+    os.makedirs(rmdir)
+
+if not os.path.exists(bindir):
+    os.makedirs(bindir)
+    print('Created directory => ' + bindir + '\n')
+
+os.chdir(wdir + '\\rm_lines')
 
 files = [i for i in glob.glob('*.{}'.format(extension))]
 
 if len(files):
     for f in files:
-        with open(wdir + 'bin/new_' + f, 'w+') as output_file:
-            with open(wdir + 'rm_lines/' + f) as input_file:
+        with open(bindir + '\\new_' + f, 'w+', newline='') as output_file:
+            with open(rmdir + '\\' + f) as input_file:
                 writer = csv.writer(
                     output_file, delimiter=',', quotechar='\"', quoting=csv.QUOTE_NONNUMERIC)
                 reader = csv.reader(input_file)
@@ -27,10 +38,15 @@ if len(files):
                 input_file.close()
             output_file.close()
 
-os.chdir(wdir + 'bin')
+print('Lines removed.\n')
+
+os.chdir(bindir)
 
 files_for_merge = [j for j in glob.glob('*.{}'.format(extension))]
 combined_csv = pd.concat(
     [pd.read_csv(f1) for f1 in files_for_merge])
-combined_csv.to_csv(wdir + "combined_csv.csv",
+merged_name = 'merged_' + time.strftime("%Y%m%d-%H%M%S")
+combined_csv.to_csv(wdir + "\\" + merged_name + '.csv',
                     index=False, encoding='utf-8-sig')
+
+print('CSV merged as ' + wdir + '\\' + merged_name + '\n')
